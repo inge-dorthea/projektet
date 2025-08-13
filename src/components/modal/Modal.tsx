57cdbad06110a9e60b9b56040a21b5fb6821bd92
inge-dorthea <201717792+inge-dorthea@users.noreply.github.com>
2025-08-13 13:20:07 +0200

    250813: Modal

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+"use client";
 
-const Modal = () => {
-  return (
-    <div>Modal</div>
-  )
+import ReactDOM from "react-dom";
+import { useEffect, useState } from "react";
+
+interface Props {
+  open: boolean;
+  id: number | null;
+  close: () => void;
 }
 
-export default Modal
+const Modal = ({ open, id, close }: Props) => {
+  // making sure the document is loaded before using createPortal, as it needs the document to work
+  const [documentLoaded, setDocumentLoaded] = useState(false);
+
+  useEffect(() => {
+    setDocumentLoaded(true);
+  }, []);
+
+  return documentLoaded
+    ? ReactDOM.createPortal(
+        <div
+          onClick={close}
+          className={`${
+            open ? "block" : "hidden"
+          } fixed top-0 left-0 right-0 bottom-0 bg-[#6E4E32]/90`}
+        >
+          <div
+            onClick={(event) => event.stopPropagation()}
+            className={`bg-gradient-to-b from-gray-200 ${
+              id == 1
+                ? "to-[#abcfe6] to-99%"
+                : id == 2
+                ? "to-[#bfddef] to-99%"
+                : " to-[#ffb472] to-99%"
+            } fixed min-h-[80%] bottom-[10vh] md:top-[10vh] left-[10vw] right-[10vw] md:right-[40vw] lg:right-[55vw] rounded-xs shadow-2xl p-5`}
+          >
+            <div
+              onClick={close}
+              className="w-fit h-fit rounded-full px-2 cursor-pointer bg-gray-800 text-gray-200 absolute top-[10px] right-[10px]"
+            >
+              X
+            </div>
+          </div>
+        </div>,
+        document.body
+      )
+    : null;
+};
+
+export default Modal;
--- src/components/modal/ModalLink.tsx
+++ src/components/modal/ModalLink.tsx
@@ -3,8 +3,12 @@
 import { getData } from "@/app/data";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import Modal from "./Modal";
 
 const ModalLink = () => {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [modalId, setModalId] = useState<number | null>(null)
+
   const [data, setData] = useState<Datatype | null>(null);
 
   useEffect(() => {
@@ -14,15 +18,23 @@ const ModalLink = () => {
     })();
   }, []);
 
-  const openModal = () => {
+  const openModal = (id: number) => {
     console.log("modal is open");
+
+    setModalOpen(true);
+    setModalId(id);
   };
 
+  const closeModal = () => {
+    console.log("modal is closed");
+    setModalOpen(false);
+  }
+
   return (
-    <div className="flex justify-center md:justify-start md:flex-row-reverse gap-4 my-2">
+    <div className="flex justify-center md:justify-start flex-row-reverse gap-4 my-2">
       {data &&
         data.data.map((item, index) => (
-          <div key={index} onClick={openModal} className="cursor-pointer">
+          <div key={index} onClick={()=> openModal(item.id)} className="cursor-pointer">
             <figure>
               <Image
                 src={"/images/db_images/" + item.sm_img}
@@ -30,10 +42,14 @@ const ModalLink = () => {
                 width="100"
                 height="100"
               />
-              <figcaption className="text-sm tracking-[-0.09em] text-center">{item.email}</figcaption>
+              <figcaption className="text-sm tracking-[-0.09em] text-center">
+                {item.email}
+              </figcaption>
             </figure>
           </div>
         ))}
+
+      <Modal open={modalOpen} id={modalId} close={closeModal} />
     </div>
   );
 };
